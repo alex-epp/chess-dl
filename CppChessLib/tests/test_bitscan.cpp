db3e93a13faadc87d234067d5a17350ae1e0811e
Alex <aepp2@ualberta.ca>
2018-12-28 02:23:41 -0700

    Tested and fixed bitscan

---
+++ CppChessLib/tests/test_bitscan.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <cstdint>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+#include "../CppChessLib/bitscan.h"
+
+
+namespace tests
+{
+	TEST_CLASS(TestBitMask)
+	{
+	public:
+
+		TEST_METHOD(TestFirst)
+		{
+			for (unsigned long i = 0; i < 64; ++i)
+				Assert::AreEqual(i, bitscan::first(UINT64_C(1) << i));
+
+			Assert::AreEqual(-1UL, bitscan::first(UINT64_C(0)));
+		}
+
+		TEST_METHOD(TestNext)
+		{
+			constexpr auto bits = UINT64_C(0b10011101001);
+			Assert::AreEqual(3UL, bitscan::next(0, bits));
+			Assert::AreEqual(5UL, bitscan::next(3, bits));
+			Assert::AreEqual(6UL, bitscan::next(5, bits));
+			Assert::AreEqual(7UL, bitscan::next(6, bits));
+			Assert::AreEqual(10UL, bitscan::next(7, bits));
+			Assert::AreEqual(-1UL, bitscan::next(10, bits));
+		}
+
+		TEST_METHOD(TestForEach)
+		{
+			constexpr auto bits = UINT64_C(0b10011101001);
+			constexpr unsigned long expected_index[6] = { 0, 3, 5, 6, 7, 10 };
+			int bit = 0;
+			BITSCAN_FOREACH(bits, index) {
+				Assert::AreEqual(expected_index[bit++], index);
+			}
+			Assert::AreEqual(6, bit);
+		}
+	};
+}
