6c5c50e75e661706b4f0c3d0db03de693b8f044d
Alex Epp <aepp2@ualberta.ca>
2020-03-12 23:00:58 -0600

    Add nodiscard attributes to Square's methods

--- backend/square.hpp
+++ backend/square.hpp
@@ -41,16 +41,16 @@ namespace chess {
 		}
 
 	public: // Data-access
-		constexpr Rank rank() const {
+        [[nodiscard]] constexpr Rank rank() const {
 			return static_cast<Rank>(this->square / 8);
 		}
-		constexpr File file() const {
+        [[nodiscard]] constexpr File file() const {
 		    return static_cast<File>(this->square % 8);
 		}
-		constexpr unsigned int get() const {
+        [[nodiscard]] constexpr unsigned int get() const {
 		    return this->square;
 		}
-		constexpr bool is_empty() const {
+        [[nodiscard]] constexpr bool is_empty() const {
 		    return this->square == Square::EMPTY;
 		}
 
@@ -65,73 +65,73 @@ namespace chess {
 		constexpr static int COMPASS_ROSE[9] = { 8, 9, 1, -7, -8, -9, -2, 7 };
 
 	public: // Operations
-		constexpr Square north() const {
+        [[nodiscard]] constexpr Square north() const {
 			return this->square + COMPASS_ROSE[NORTH];
 		}
-		constexpr Square east() const {
+        [[nodiscard]] constexpr Square east() const {
 			return this->square + COMPASS_ROSE[EAST];
 		}
-		constexpr Square south() const {
+        [[nodiscard]] constexpr Square south() const {
 			return this->square + COMPASS_ROSE[SOUTH];
 		}
-		constexpr Square west() const {
+        [[nodiscard]] constexpr Square west() const {
 			return this->square + COMPASS_ROSE[WEST];
 		}
-		constexpr Square northeast() const {
+        [[nodiscard]] constexpr Square northeast() const {
 			return this->square + COMPASS_ROSE[NORTHEAST];
 		}
-		constexpr Square southeast() const {
+        [[nodiscard]] constexpr Square southeast() const {
 			return this->square + COMPASS_ROSE[SOUTHEAST];
 		}
-		constexpr Square southwest() const {
+        [[nodiscard]] constexpr Square southwest() const {
 			return this->square + COMPASS_ROSE[SOUTHWEST];
 		}
-		constexpr Square northwest() const {
+        [[nodiscard]] constexpr Square northwest() const {
 			return this->square + COMPASS_ROSE[NORTHWEST];
 		}
 
-		constexpr Square north(const int amount) const {
+        [[nodiscard]] constexpr Square north(const int amount) const {
 			return this->square + COMPASS_ROSE[NORTH] * amount;
 		}
-		constexpr Square east(const int amount) const {
+        [[nodiscard]] constexpr Square east(const int amount) const {
 			return this->square + COMPASS_ROSE[EAST] * amount;
 		}
-		constexpr Square south(const int amount) const {
+        [[nodiscard]] constexpr Square south(const int amount) const {
 			return this->square + COMPASS_ROSE[SOUTH] * amount;
 		}
-		constexpr Square west(const int amount) const {
+        [[nodiscard]] constexpr Square west(const int amount) const {
 			return this->square + COMPASS_ROSE[WEST] * amount;
 		}
-		constexpr Square northeast(const int amount) const {
+        [[nodiscard]] constexpr Square northeast(const int amount) const {
 			return this->square + COMPASS_ROSE[NORTHEAST] * amount;
 		}
-		constexpr Square southeast(const int amount) const {
+        [[nodiscard]] constexpr Square southeast(const int amount) const {
 			return this->square + COMPASS_ROSE[SOUTHEAST] * amount;
 		}
-		constexpr Square southwest(const int amount) const {
+        [[nodiscard]] constexpr Square southwest(const int amount) const {
 			return this->square + COMPASS_ROSE[SOUTHWEST] * amount;
 		}
-		constexpr Square northwest(const int amount) const {
+        [[nodiscard]] constexpr Square northwest(const int amount) const {
 			return this->square + COMPASS_ROSE[NORTHWEST] * amount;
 		}
 
-		constexpr Square forward(Piece::Colour colour) const {
+        [[nodiscard]] constexpr Square forward(Piece::Colour colour) const {
 			auto dir = (colour == Piece::WHITE) ? 1 : -1;
 			return this->north(dir);
 		}
-		constexpr Square backward(Piece::Colour colour) const {
+        [[nodiscard]] constexpr Square backward(Piece::Colour colour) const {
 			auto dir = (colour == Piece::WHITE) ? 1 : -1;
 			return this->south(dir);
 		}
 
-		constexpr Square flip_vertical() {
+        [[nodiscard]] constexpr Square flip_vertical() const {
 			if (this->square == EMPTY)
 				return EMPTY;
 			else
 				return this->square ^ 56;
 		}
 
-		constexpr bool operator == (const Square rhs) const {
+        [[nodiscard]] constexpr bool operator == (const Square rhs) const {
 			return this->square == rhs.square;
 		}
 
